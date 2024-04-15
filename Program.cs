55c1d9a2ce596c33852101582ccfc9f194791c59
Jay <josephjones.canangga@gmail.com>
2024-04-15 16:21:23 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using Blazored.Modal;
+using GoPlayAsiaWebApp;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddBlazoredModal();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
