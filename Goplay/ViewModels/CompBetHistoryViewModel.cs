8614ddbe1e8e4b8fcc1fc56e4696611b603f8a23
cjpvaquilar <christinejoyvaquilar@gmail.com>
2024-09-02 17:08:02 +0800

    user bet history

--- Goplay/ViewModels/CompBetHistoryViewModel.cs
+++ Goplay/ViewModels/CompBetHistoryViewModel.cs
@@ -25,6 +25,15 @@ public class CompBetHistoryViewModel : BaseViewModel
             _gameRounds = value;
         }
     }
+    public ObservableCollection<UserBetHistoryResultDTO> FilteredGameRounds { get; set; }
+    public List<UserBetHistoryResultDTO> VisibleGameRounds { get; set; } = new List<UserBetHistoryResultDTO>();
+    public bool ShowSeeMoreButton { get; set; } = false;
+    public bool ShowAllRows { get; set; } = false;
+    public int DisplayCnt { get; set; } = 20;
+
+    public string Category { get; set; }
+    public DateTime SelDate { get; set; }
+    
     #endregion
 
     #region Life cycle methods
@@ -42,13 +51,64 @@ public class CompBetHistoryViewModel : BaseViewModel
     {
         ReportParamsDTO paramsModel = new ReportParamsDTO()
         {
-            UserId = _currentUser.Id
+            UserId = _currentUser.Id,
+            Date = SelDate,
+            TimeFrom = TimeSpan.Zero,
+            TimeTo = new TimeSpan(23, 59, 59, 999)
         };
         var gameRounds = await _reportService.GetGameHistoryWithUserBets(paramsModel, _currentUser.Token);
         if (gameRounds != null && gameRounds.GameRounds != null && gameRounds.GameRounds.Count > 0)
             GameRounds = new ObservableCollection<UserBetHistoryResultDTO>(gameRounds.GameRounds);
         else
             GameRounds = new ObservableCollection<UserBetHistoryResultDTO>();
+        FilterGameRounds();
     }
+
+    private void FilterGameRounds()
+    {
+        if (GameRounds != null)
+        {
+            if (string.IsNullOrEmpty(Category))
+            {
+                FilteredGameRounds = new ObservableCollection<UserBetHistoryResultDTO>(GameRounds);
+            }
+            else
+            {
+                var filteredList = GameRounds.Where(gr => gr.GameCategory.ToUpper() == Category.ToUpper()).ToList();
+                FilteredGameRounds = new ObservableCollection<UserBetHistoryResultDTO>(filteredList);
+            }
+
+            if (FilteredGameRounds.Count > 20 && !ShowAllRows)
+            {
+                ShowSeeMoreButton = true;
+            }
+            else
+            {
+                ShowSeeMoreButton = false;
+            }
+
+            UpdateVisibleGameRounds();
+        }
+    }
+
+    public void UpdateVisibleGameRounds()
+    {
+        if (ShowAllRows)
+        {
+            VisibleGameRounds = FilteredGameRounds.ToList();
+        }
+        else
+        {
+            VisibleGameRounds = FilteredGameRounds.Take(DisplayCnt).ToList();
+        }
+    }
+
+    public async Task SetCategory(string param)
+    {
+        Category = param;
+        FilterGameRounds();
+        await CallInvoke();
+    }
+
     #endregion
 }
--- GoplayasiaSharedKernel/Models/Constants/Settings.cs
+++ GoplayasiaSharedKernel/Models/Constants/Settings.cs
@@ -295,6 +295,7 @@
             #endregion
 
             #region eGAMES GAMETYPE
+            public const string specialty = "specialty";
             public const string egamesAll = "all";
             public const string egamesLive = "live";
             public const string egamesRng = "rng";
